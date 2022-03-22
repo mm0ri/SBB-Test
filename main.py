030d327e93934bb7befbaf9d486dac65aab594e3
Vadim <Shkuratvadim@gmail.com>
2022-03-23 01:32:26 +0300

    start page
    Added start page response.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = FastAPI()
 
 res_list = file_init()
 
+@app.get("/", status_code=200)
+async def start_page():
+    return {"message": "please, make response by using parametr /cards/'bank card number'"}
+
 @app.get("/cards/{card_number}", status_code=200)
 async def read_item(card_number):
     return data_check(card_init(card_number), res_list,)
