ac24f5a95fd4c18cc178d080e1f4074b0dcd453a
Vadim <Shkuratvadim@gmail.com>
2022-03-23 01:54:57 +0300

    some changes
    Some changes in description.

--- func.py
+++ func.py
@@ -17,15 +17,17 @@ def card_init(card_number):
         n_card_number = card_number[0:6]
         return n_card_number
     elif card_number.isdigit() == False:
-        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number type. Its should contains only numbers.')
+        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number type. Its should contain only numbers.')
     elif len(card_number) < 16:
         raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number size. Its size should be more than 16.')
     elif len(card_number) > 20:
         raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number size. Its size should be less than 20.')
     else:
-        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number. Its size should be more than 16 and less that 20 digits and contains only numbers. And no free space in the number')
+        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number. Its size should be more than 16 and less that 20 digits and contain only numbers. And no free space in the number')
 
 def data_check(n_card_number, main_l):
     for row in main_l:
         if row[0] == n_card_number:
             return(row)
+        else:
+            return 'Card number not in database.'
--- main.py
+++ main.py
@@ -7,7 +7,11 @@ res_list = file_init()
 
 @app.get("/", status_code=200)
 async def start_page():
-    return {"message": "please, make response by using parametr /cards/'bank card number'"}
+    return "Please, make response by using parametr /cards/'bank card number'"
+
+@app.get("/cards/", status_code=200)
+async def card_page():
+    return "Card number should have size more than 16 and less than 20 digits. Also its should contain only numbers withous free space."
 
 @app.get("/cards/{card_number}", status_code=200)
 async def read_item(card_number):
