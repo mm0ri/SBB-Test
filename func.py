e63cb79338de1ea113a683b3ba261ad45c056c9c
Vadim <Shkuratvadim@gmail.com>
2022-03-24 23:48:05 +0300

    func updated
    Changed description of raising exception.

--- func.py
+++ func.py
@@ -5,9 +5,9 @@ from fastapi import HTTPException
 from pathlib import Path
 
 home = os.getcwd()
-path_to_file = Path(home, "src", "binlist-data.csv")
+path_to_file = Path(home, "src", "binlist-data.csv") # Actually, should work, but Sometimes has problems with path.
 
-#path_to_file = 'd:/User Projects/SBB-Test/src/binlist-data.csv'
+#path_to_file = 'd:/User Projects/SBB-Test/src/binlist-data.csv' # If you have problems with path, you should use this line with your own path to src folder in project folder.
 
 def file_init():
     main_l = []
@@ -26,13 +26,13 @@ def card_init(card_number):
         raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number type. Its should contain only numbers.')
     elif len(card_number) < 16:
         status_code = 500
-        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number size. Its size should be more than 16.')
+        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number size. Its size should be greater than or equal to 16.')
     elif len(card_number) > 20:
         status_code = 500
-        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number size. Its size should be less than 20.')
+        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number size. Its size should be less than or equal to 20.')
     else:
         status_code = 500
-        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number. Its size should be more than 16 and less that 20 digits and contain only numbers. And no free space in the number')
+        raise HTTPException(status_code, detail=f'Status code: {status_code}. Wrong card number. Its size should be greater than or equal 16 and less than or equal to 20 digits and contain only numbers. And has no free space in the number')
 
 def data_check(n_card_number, main_l):
     for row in main_l:
